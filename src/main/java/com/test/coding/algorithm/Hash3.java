c9728535d6c0d53b543fc9afaa68cbdb176ba8b2
choijihye <choijh92@redwoodk.com>
2024-02-25 13:26:18 +0900

    20240225 프로그래머스 해시3

---
+++ src/main/java/com/test/coding/algorithm/Hash3.java
@@ -0,0 +1,34 @@
+package com.test.coding.algorithm;
+
+import java.util.HashSet;
+import java.util.List;
+
+
+/**
+ * 프로그래머스
+ * 알고리즘 > 해시
+ * https://school.programmers.co.kr/learn/courses/30/lessons/1845
+ */
+public class Hash3 {
+
+    public static void main(String[] args) {
+
+        Hash3 hash3 = new Hash3();
+        Boolean result = hash3.solution(new String[]{"123","456","123789"});
+        System.out.println("result = " + result);
+    }
+
+    public Boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        HashSet<String> hashSet = new HashSet<>(List.of(phone_book));
+        for (String s: hashSet){
+            for (int i = 1; i < s.length(); i++){
+                if (hashSet.contains(s.substring(0, i))){
+                    return false;
+                }
+            }
+        }
+        return answer;
+    }
+}
